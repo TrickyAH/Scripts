35889eb10acd966f2e6e4eb6417709ee83bb4843
1xAbel <111407812+1xAbel@users.noreply.github.com>
2023-04-09 19:36:46 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,11 @@
+local games = {
+    [{12135645852}] = "https://raw.githubusercontent.com/TrickyAH/Scripts/main/Anime%20Catching%20Simulator%20Exploit",
+    [{9031522337}] = "https://raw.githubusercontent.com/TrickyAH/Scripts/main/Rpg%20Champions%20Exploit",
+    [{9447079542}] = "https://raw.githubusercontent.com/TrickyAH/Scripts/main/project_mugetsu.lua",
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))(); break
+    end
+end
