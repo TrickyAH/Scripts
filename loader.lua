5e427ad107893bc56232cd869a60232d8aa6e171
1xAbel <111407812+1xAbel@users.noreply.github.com>
2023-04-10 06:07:08 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,14 @@
 local games = {
-    [{12135645852}] = "https://raw.githubusercontent.com/TrickyAH/Scripts/main/Anime%20Catching%20Simulator%20Exploit",
-    [{9031522337}] = "https://raw.githubusercontent.com/TrickyAH/Scripts/main/Rpg%20Champions%20Exploit",
-    [{9474703390, 10202329527, 11420877323}] = "https://raw.githubusercontent.com/TrickyAH/Scripts/main/project_mugetsu.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/11420877323.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/10202329527.lua"
+    [{12135645852}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/Anime%20Catching%20Simulator%20Exploit"},
+    [{9031522337}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/Rpg%20Champions%20Exploit"},
+    [{9474703390, 10202329527, 11420877323}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/project_mugetsu.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/11420877323.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/10202329527.lua"}
 }
 
-for ids, url in next, games do
+for ids, urls in next, games do
     if table.find(ids, game.PlaceId) then
-        loadstring(game:HttpGet(url))(); break
+        for _, url in ipairs(urls) do
+            loadstring(game:HttpGet(url))();
+        end
+        break
     end
 end
