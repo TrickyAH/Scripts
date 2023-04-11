564641c97fca4978e19cceb2a6efb32fb92cce29
1xAbel <111407812+1xAbel@users.noreply.github.com>
2023-04-11 00:29:06 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local games = {
     [{12135645852}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/Anime%20Catching%20Simulator%20Exploit"},
     [{9031522337}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/Rpg%20Champions%20Exploit"},
-    [{9474703390, 10202329527, 11420877323, 9799727321}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/project_mugetsu.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/11420877323.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/10202329527.lua"}
+    [{9474703390, 10202329527, 11420877323, 9799727321}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/project_mugetsu.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/11420877323.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/10202329527.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/9799727321.lua"}
 }
 
 for ids, urls in next, games do
