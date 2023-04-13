16bcffa298fee73f940bbf04b0c145ca3238c051
1xAbel <111407812+1xAbel@users.noreply.github.com>
2023-04-13 17:18:42 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local games = {
     [{12135645852}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/Anime%20Catching%20Simulator%20Exploit"},
     [{9031522337}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/Rpg%20Champions%20Exploit"},
-    [{9474703390, 10202329527, 11420877323, 9799727321}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/project_mugetsu.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/9799727321.lua"},
+    [{9474703390, 10202329527, 13117265227, 9799727321}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/project_mugetsu.lua", "https://raw.githubusercontent.com/TrickyAH/Scripts/main/9799727321.lua"},
     [{8651781069}] = {"https://raw.githubusercontent.com/TrickyAH/Scripts/main/Voxlblade.lua"},
 }
 
